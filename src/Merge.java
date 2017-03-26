7cf7615592f4a0b2aab9a298c8a354862644840d
Amin Javed <ajaved@student.tgm.ac.at>
2017-03-26 11:06:49 +0200

    6x Merge

--- src/Merge.java
+++ src/Merge.java
@@ -2,7 +2,7 @@
 public class Merge {
 
 	public static void main(String[] args) {
-		for( int i = 0 ; i < 5 ; i++ ) {
+		for( int i = 0 ; i < 6 ; i++ ) {
 			System.out.println("Merge + i");
 		}
 	}
